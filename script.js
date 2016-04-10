a7c4bb69beeed6f437ecb69325939164cf6ac7e6
Armando Alejandro <aalejandro1708@gmail.com>
2016-04-10 11:18:55 -0500

    Creado arreglo de objetos

---
+++ script.js
@@ -0,0 +1,65 @@
+//Armado Alejandro
+//Derechos reservados
+
+//Arreglo de objetos
+
+var matrizEstudiantes=[
+	{
+		nombre:"Leo",
+		genero:"Masculino";
+		edad:28,
+		notas: [100,100,99,99],
+		ciudad:"Santa Elena",
+		lenguajeFavorito:"JavaScript",
+		laborando: true
+	},
+
+	{
+		nombre:"Hugo",
+		genero:"Masculino";
+		edad:5,
+		notas: [100,95,99,100],
+		ciudad:"Santa Elena",
+		lenguajeFavorito:"C++",
+		laborando: false
+	},
+
+	{
+		nombre:"Miriam",
+		genero:"Femenino";
+		edad:25,
+		notas: [100,100,75,94],
+		ciudad:"Santa Elena",
+		lenguajeFavorito:"Español",
+		laborando: false
+	},
+
+	{
+		nombre:"Karen",
+		genero:"Femenino";
+		edad:23,
+		notas: [70,76,99,100],
+		ciudad:"La Libertad",
+		lenguajeFavorito:"Java",
+		laborando: true
+	},
+
+	{
+		nombre:"Luis",
+		genero:"Masculino";
+		edad:3,
+		notas: [100,100,100,100],
+		ciudad:"Santa Elena",
+		lenguajeFavorito:"VB",
+		laborando: false
+	},	
+
+];
+
+
+		console.log(matrizEstudiantes.length);
+
+
+
+
+
